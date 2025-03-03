00926e972d9179c87bee97aa79e8b9fcd9081cc6
Sweextly <kissisluvs@gmail.com>
2025-03-03 16:46:50 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const lotus = document.querySelector('.lotus');
+    lotus.addEventListener('mouseover', () => {
+        lotus.classList.add('hover');
+    });
+    lotus.addEventListener('mouseout', () => {
+        lotus.classList.remove('hover');
+    });
+});
+
+if (condition) {
+    
+}
